7776416108ee580a6a20c98824df275561356c3d
Torbary <torbarey020@gmail.com>
2023-04-23 19:53:27 +0100

    modified

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,9 @@
-# create a file
+# Create file in /tmp
 
-
-file { '/tmp/school':
-  path    => 'tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
+file { '/tmp/school' :
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    }
