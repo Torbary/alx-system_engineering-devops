0a1b703906ea6bb3cdb34ae6ded6347a23f4976f
Torbary <torbarey020@gmail.com>
2023-05-23 12:53:07 +0100

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,56 +1,19 @@
 #!/usr/bin/python3
-"""Export to JSON"""
-
-
-import sys
+"""Exports to-do list information for a given employee ID to JSON format."""
 import json
 import requests
-
-
-def export_employee_todo_json(employee_id):
-    """
-    Export employee's TODO list data in JSON format.
-
-    Args:
-        employee_id (int): The ID of the employee.
-
-    Returns:
-        None
-    """
-    employee_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    employee_response = requests.get(employee_url)
-
-    if employee_response.status_code == 200:
-        employee_data = employee_response.json()
-        employee_name = employee_data['username']
-
-        todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
-        todo_response = requests.get(todo_url)
-        if todo_response.status_code == 200:
-            todo_data = todo_response.json()
-
-            json_data = {str(employee_id): []}
-            for task in todo_data:
-                json_data[str(employee_id)].append({
-                    "task": task['title'],
-                    "completed": task['completed'],
-                    "username": employee_name
-                })
-
-            filename = "{}.json".format(employee_id)
-            with open(filename, 'w') as json_file:
-                json.dump(json_data, json_file)
-
-            print("JSON file '{}' has been created.".format(filename))
-        else:
-            print("Error: Could not fetch TODO data for employee ID {}".format(employee_id))
-    else:
-        print("Error: Could not fetch employee data for ID {}".format(employee_id))
-
+import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py employee_id")
-    else:
-        employee_id = int(sys.argv[1])
-        export_employee_todo_json(employee_id)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": username
+            } for t in todos]}, jsonfile)
