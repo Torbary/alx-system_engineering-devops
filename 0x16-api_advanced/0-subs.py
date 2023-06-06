dce6733ec6eba71b682cd1d53a95f747eac94ddb
Torbary <torbarey020@gmail.com>
2023-06-06 17:00:44 +0100

    add docs to module

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Query Reddit API to determine subreddit sub count
+"""
 
 import requests
 
@@ -15,11 +17,14 @@ def number_of_subscribers(subreddit):
         int: The number of subscribers for the subreddit.
         Returns 0 if the subreddit is invalid.
     """
+    # set custom user_agent
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     user_agent = "torbarey"
+
+    # custom user_agent avoids requests limit
     headers = {"User-Agent": user_agent}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
